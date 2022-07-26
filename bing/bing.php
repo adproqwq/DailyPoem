7a4da1fa5ccbb023d13fdf9417fd6bd5123c79da
gadpro <101449500+gadpro@users.noreply.github.com>
2022-07-27 03:55:07 +0800

    Update bing.php

--- bing/bing.php
+++ bing/bing.php
@@ -1,10 +1,7 @@
 
 <?php
-$str = file_get_contents('http://cn.bing.com/HPImageArchive.aspx?idx=0&n=1');   //从bing获取数据
-if(preg_match("/<url>(.+?)<\/url>/ies",$str,$matches)){ //正则匹配抓取图片url
-    $imgurl='http://cn.bing.com'.$matches[1];
-}else{  //如果由于某些原因，没抓取到图片地址
-    $imgurl='http://img.infinitynewtab.com/InfinityWallpaper/2_14.jpg'; //使用默认的图像(默认图像链接可修改为自己的)
-}
-header("Location: $imgurl");    //header跳转
+    $str = file_get_contents('http://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1'); //读取必应api，获得相应数据
+    $str = json_decode($str,true);
+    $imgurl = 'http://cn.bing.com'.$str['images'][0]['url'];    //获取图片url
+    $img = grabImage($imgurl, $path.'/'.$filename); //读取并保存图片
 ?>
