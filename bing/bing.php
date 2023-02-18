eb169c1e3ac38ebc0439c026d56f142118583142
gadpro <101449500+gadpro@users.noreply.github.com>
2023-02-18 14:25:47 +0800

    Delete bing directory

--- bing/bing.php
+++
@@ -1,7 +0,0 @@
-
-<?php
-    $str = file_get_contents('http://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1'); //读取必应api，获得相应数据
-    $str = json_decode($str,true);
-    $imgurl = 'http://cn.bing.com'.$str['images'][0]['url'];    //获取图片url
-    $img = grabImage($imgurl, $path.'/'.$filename); //读取并保存图片
-?>
