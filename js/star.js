7dffeb1e13695d125559bf67abcdc7286cda4bf5
adproqwq <101449500+adproqwq@users.noreply.github.com>
2023-11-06 17:57:27 +0800

    新增收藏模块；优化文件结构

---
+++ js/star.js
@@ -0,0 +1,69 @@
+function star(poem, poem_info){
+    var star;
+    if(self.localStorage.getItem("star") != null){
+        star = JSON.parse(self.localStorage.getItem("star"));
+        star.stared[star.stared.length] = poem + poem_info;
+        self.localStorage.setItem("star",JSON.stringify(star));
+    }
+    else{
+        star = {
+            stared: [poem + poem_info]
+        }
+        self.localStorage.setItem("star",JSON.stringify(star))
+    }
+};
+
+function removeStared(index){
+    var star;
+    star = JSON.parse(self.localStorage.getItem("star"));
+    star['stared'].splice(index,1);
+    console.log(star['stared']);
+    self.localStorage.setItem("star",JSON.stringify(star));
+    getStared();
+};
+
+function copyStared(index){
+    var star;
+    star = JSON.parse(self.localStorage.getItem("star"));
+    navigator.clipboard.writeText(star['stared'][index]).then(() => {
+        alert('已复制到剪切板，请自行粘贴');
+    });
+};
+
+function getStared(){
+    var star, str = '';
+    if(self.localStorage.getItem("star") != null){
+        star = JSON.parse(self.localStorage.getItem("star"));
+        for(var i = 0; i < star.stared.length; i++){
+            str += `
+                <tr>
+                    <td>${star['stared'][i]}</td>
+                    <td><button name='copy' index=${String(i)}>复制</button><button name='remove' index=${String(i)}>删除</button></td>
+                </tr>
+            `;
+        };
+        var aTbody = document.querySelector('tbody');
+        aTbody.innerHTML = str;
+    }
+};
+
+var aTbody = document.querySelector('tbody');
+aTbody.addEventListener('click',function(e){
+    if(e.target.getAttribute('name') == 'remove'){
+        var index = Number(e.target.getAttribute('index'));
+        removeStared(index);
+    }
+    else if(e.target.getAttribute('name') == 'copy'){
+        var index = Number(e.target.getAttribute('index'));
+        copyStared(index);
+    }
+});
+
+/*
+{
+    stared: [
+        "xxx",
+        "yyy"
+    ]
+}
+*/
