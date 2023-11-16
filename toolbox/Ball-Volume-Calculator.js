dfad4aef4871209a12702e90c3a38fc9a74bdde2
adproqwq <101449500+adproqwq@users.noreply.github.com>
2023-11-16 20:23:39 +0800

    精简代码；删除工具箱

--- toolbox/Ball-Volume-Calculator.js
+++
@@ -1,6 +0,0 @@
-function InputAndCalculation(){
-    Radius=prompt("请输入球的半径");
-    Pai=prompt("请输入π的取值")
-    CalculationResult=4/3*parseFloat(Pai)*Math.pow(Number(Radius),3);
-    alert("球的体积是：" + CalculationResult);
-};
--- toolbox/Grid-Polygon-Area-Calculator.js
+++
@@ -1,6 +0,0 @@
-function InputAndCalculation(){
-    IGPN=prompt("请输入格点多边形内部格点数");
-    BGPN=prompt("请输入格点多边形边界格点数");
-    CalculationResult=parseInt(IGPN)+BGPN/2-1;
-    alert("格点多边形的面积是：" + CalculationResult);
-};
